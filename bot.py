3c2a54d6808495ea4b6f549941d0d2232f57ad54
Lazikg <lazikggg@gmail.com>
2025-03-30 00:25:10 +0500

    Add language selection to registration process

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from handlers import (
     help_command,
     register,
     cancel,
+    set_language,
     get_fio,
     get_phone,
     get_photo,
@@ -14,7 +15,7 @@ from handlers import (
     get_telegram,
     get_instagram,
     get_website,
-    FIO, PHONE, PHOTO, SPHERE, JOB, TELEGRAM, INSTAGRAM, WEBSITE
+    LANGUAGE, FIO, PHONE, PHOTO, SPHERE, JOB, TELEGRAM, INSTAGRAM, WEBSITE
 )
 
 # Logging ni sozlash
@@ -33,6 +34,7 @@ def main() -> None:
     registration_handler = ConversationHandler(
         entry_points=[CommandHandler("register", register)],
         states={
+            LANGUAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, set_language)],
             FIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_fio)],
             PHONE: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_phone)],
             PHOTO: [
@@ -47,8 +49,27 @@ def main() -> None:
         fallbacks=[CommandHandler("cancel", cancel)],
     )
 
+    # Start buyrug'i uchun conversation handler (til tanlash uchun)
+    start_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            LANGUAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, set_language)],
+            FIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_fio)],
+            PHONE: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_phone)],
+            PHOTO: [
+                MessageHandler(filters.PHOTO | filters.Document.IMAGE, get_photo)
+            ],
+            SPHERE: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_sphere)],
+            JOB: [MessageHandler(filters.TEXT, get_job)],
+            TELEGRAM: [MessageHandler(filters.TEXT, get_telegram)],
+            INSTAGRAM: [MessageHandler(filters.TEXT, get_instagram)],
+            WEBSITE: [MessageHandler(filters.TEXT, get_website)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+    
     # Handler(qayta ishlovchi)larni qo'shish
-    application.add_handler(CommandHandler("start", start))
+    application.add_handler(start_handler)
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(registration_handler)
 
